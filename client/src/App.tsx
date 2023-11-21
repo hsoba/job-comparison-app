369385f6499368067c07e2f0b0baf54e4e722e81
hsoba <hsobav@gmail.com>
2023-11-20 22:36:20 -0500

    displaying api data on frontend

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,28 @@
-import React from "react";
+import { type } from "os";
+import React, { useEffect, useState } from "react";
+
+interface BackEndData {
+  users: string[];
+}
 
 function App() {
-  return <div> App </div>;
+  const [backEndData, setBackEndData] = useState<BackEndData>({ users: [] });
+
+  useEffect(() => {
+    fetch("/api")
+      .then((res) => res.json())
+      .then((data) => setBackEndData(data));
+  }, []);
+
+  return (
+    <div>
+      {typeof backEndData.users === "undefined" ? (
+        <p>Loading...</p>
+      ) : (
+        backEndData.users.map((user, i) => <p key={i}>{user}</p>)
+      )}
+    </div>
+  );
 }
 
 export default App;
